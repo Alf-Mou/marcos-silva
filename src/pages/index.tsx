3f485cf0a4f8de6b3d88f9358ed85f124adc4440
Suelen Dias <suelenanjos16@gmail.com>
2025-10-27 12:50:02 -0300

    teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import Footer from "@/components/footer";
 
 
 export default function Home() {
+  console.log("teste");
+  
   return (
     <main className="w-full font-poppins flex flex-col">
       <Header/>
